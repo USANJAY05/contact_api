a1a322378aac173ebdc42dc0260347295e06b446
Sanjay <sanjay.05@icloud.com>
2026-07-12 06:58:33 +0530

    feat: implement lifespan for automatic table creation

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from src.routes.contact import route as contact_route
+from src.core.db import engine, Base
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Contact API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Create tables on startup
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+app = FastAPI(title="Contact API", lifespan=lifespan)
 
 app.include_router(contact_route)
 
